2821cc9ff0b800e4f62e0b4d1056a49ce7c41679
alexander0354 <alebrekvi@gmail.com>
2023-02-17 08:43:28 +0100

    basic html oppsett

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <h1>Oppskrifter</h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+      </>
+  );
+}
+
+export default App;
